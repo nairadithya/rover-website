f37eb5cab3bd8dee0e1a2125d3ec93108937ea9e
Adithya Nair <adithyanair121@gmail.com>
2025-06-10 13:07:36 +0530

    Updated nav, footer, socials and finally content

--- src/navigation.ts
+++ src/navigation.ts
@@ -28,9 +28,13 @@ export const headerData = {
 export const footerData = {
   links: [{}, {}, {}, {}],
   socialLinks: [
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/team_rover_avvb/' },
+    {
+      ariaLabel: 'LinkedIn',
+      icon: 'tabler:brand-linkedin',
+      href: 'https://www.linkedin.com/in/avv-b-rover-team/',
+    },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/Anveshak708Odyssey' },
   ],
 };
